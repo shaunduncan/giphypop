90db413084bc5924e31d5f9602dfb4096bedbc5b
Shaun Duncan <shaun.duncan@gmail.com>
2013-06-05 18:22:59 -0400

    Have the api endpoints in place. Needs some refactor and tests

--- giphypop.py
+++ giphypop.py
@@ -1,7 +1,5 @@
 import warnings
 
-from collections import namedtuple
-
 import requests
 
 
@@ -11,25 +9,183 @@ GIPHY_API_ENDPOINT = 'http://api.giphy.com/v1/gifs'
 GIPHY_PUBLIC_KEY = 'dc6zaTOxFJmzC'
 
 
-class GiphyApiException(Exception):
-    pass
-
-
-class GiphyResult(dict):
-
+class AttrDict(dict):
+    """
+    A subclass of dict that exposes keys as attributes
+    """
     def __getattr__(self, attr):
-        print 'Getting', attr
-        if hasattr(self, attr):
-            return getattr(self, attr)
-        else:
-            return self.get(attr, u'')
+        if attr in self.__dict__:
+            return self.__dict__[attr]
+
+        try:
+            return self[attr]
+        except KeyError:
+            raise AttributeError("'%s' object has no attribute '%s'" % (self.__class__.__name__, attr))
 
     def __setattr__(self, attr, value):
-        print 'Setting', attr
-        if hasattr(self, attr):
-            super(GiphyResult, self).__setattr__(attr)
+        if attr in self.__dict__:
+            self.__dict__[attr] = value
         else:
             self[attr] = value
+
+
+class GiphyResult(AttrDict):
+    """
+    A special case AttrDict that handles data specifically being returned
+    from the giphy api (i.e. integer values converted from strings). The structure
+    is very object-like, but retains all the qualities of a python dict.
+    Attributes are not a direct mirror of giphy api results, but follow this pattern::
+
+        <Result Object>
+            - id
+            - type: image type (i.e. gif)
+            - url: URL to giphy page
+            - raw_data: copy of original data response from giphy (JSON)
+            - fullscreen: bit.ly link to giphy fullscreen gif
+            - tiled: bit.ly link to giphy tiled gif
+            - bitly: bit.ly version of `url`
+            - media_url: URL directly to image (original size)
+            - frames: number of frames
+            - height: image height (original image)
+            - width: image width (original image)
+            - size: filesize (in bytes, original image)
+            - fixed_height: (variable width @ 200px height)
+                - url: URL directly to image
+                - width: image width
+                - height: image height
+                - downsampled:
+                    - url: URL directly to image
+                    - width: image width
+                    - height: image height
+                - still: (a still image of gif)
+                    - url: URL directly to image
+                    - width: image width
+                    - height: image height
+            - fixed_width: (variable height @ 200px width)
+                - url: URL directly to image
+                - width: image width
+                - height: image height
+                - downsampled:
+                    - url: URL directly to image
+                    - width: image width
+                    - height: image height
+                - still: (a still image of gif)
+                    - url: URL directly to image
+                    - width: image width
+                    - height: image height
+    """
+    def __init__(self, data=None):
+        if data:
+            super(GiphyResult, self).__init__(id=data.get('id'),
+                                              url=data.get('url'),
+                                              type=data.get('type'),
+                                              raw_data=data)
+
+            # bit.ly urls
+            self.fullscreen = data.get('bitly_fullscreen_url')
+            self.tiled = data.get('bitly_tiled_url')
+            self.bitly = data.get('bitly_gif_url')
+
+            # Shorthand
+            self._make_images(data.get('images', {}))
+
+    @property
+    def media_url(self):
+        """
+        The media URL of the gif at its original size
+        """
+        return self.original.url
+
+    @property
+    def frames(self):
+        """
+        The number of frames of the gif
+        """
+        return self.original.frames
+
+    @property
+    def width(self):
+        """
+        The width of the gif at its original size
+        """
+        return self.original.width
+
+    @property
+    def height(self):
+        """
+        The height of the gif at its original size
+        """
+        return self.original.height
+
+    @property
+    def filesize(self):
+        """
+        The size of the original size file in bytes
+        """
+        return self.original.size
+
+    def _make_images(self, images):
+        """
+        Takes an image dict from the giphy api and converts it to attributes.
+        Any fields expected to be int (width, height, size, frames) will be attempted
+        to be converted. Also, the keys of `data` serve as the attribute names, but
+        with special action taken. Keys are split by the last underscore; anything prior
+        becomes the attribute name, anything after becomes a sub-attribute. For example:
+        fixed_width_downsampled will end up at `self.fixed_width.downsampled`
+        """
+        # Order matters :)
+        process = ('original',
+                   'fixed_width',
+                   'fixed_height',
+                   'fixed_width_downsampled',
+                   'fixed_width_still',
+                   'fixed_height_downsampled',
+                   'fixed_height_still')
+
+        for key in process:
+            data = images.get(key)
+
+            # Ignore empties
+            if not data:
+                continue
+
+            parts = key.split('_')
+
+            # attr/subattr style
+            if len(parts) > 2:
+                attr, subattr = '_'.join(parts[:-1]), parts[-1]
+            else:
+                attr, subattr = '_'.join(parts), None
+
+            # Normalize data
+            img = AttrDict(self._normalized(data))
+
+            if subattr is None:
+                setattr(self, attr, img)
+            else:
+                setattr(getattr(self, attr), subattr, img)
+
+    def _normalized(self, data):
+        """
+        Does a normalization of sorts on image type data so that values
+        that should be integers are converted from strings
+        """
+        int_keys = ('frames', 'width', 'height', 'size')
+
+        for key in int_keys:
+            if key not in data:
+                continue
+
+            try:
+                data[key] = int(data[key])
+            except ValueError:
+                pass  # Ignored
+
+        return data
+
+
+class GiphyApiException(Exception):
+    pass
 
 
 class Giphy(object):
@@ -40,8 +196,7 @@ class Giphy(object):
     def __init__(self, api_key=GIPHY_PUBLIC_KEY):
         # Warn if using public key
         if api_key == GIPHY_PUBLIC_KEY:
-            warnings.warn('The giphy public key is for beta testing the api and may be deprecated',
-                          PendingDeprecationWarning)
+            warnings.warn(u'You are using the giphy public api key. This should be used for testing only')
 
         self.api_key = api_key
 
@@ -51,21 +206,6 @@ class Giphy(object):
     def _check_or_raise(self, meta):
         if meta.get('status') != 200:
             raise GiphyApiException(meta.get('error_message'))
-
-    def _make_result(self, data):
-        result = GiphyResult(id=data.get('id'),
-                             url=data.get('url'),
-                             type=data.get('type'))
-
-        # bitly urls
-        result.bitly = GiphyResult(fullscreen=data.get('bitly_fullscreen_url'),
-                                   tiled=data.get('bitly_tiled_url'),
-                                   gif=data.get('bitly_gif_url'))
-
-        images = data.get('images', {})
-        result.images = GiphyResult((k, GiphyResult(**v)) for k, v in images.iteritems())
-
-        return result
 
     def search(self, term=None, phrase=None, limit=None, offset=0):
         """
@@ -94,6 +234,8 @@ class Giphy(object):
         params = {
             'q': term or phrase,
             'api_key': self.api_key,
+            'offset': offset,
+            'limit': limit,
         }
 
         resp = requests.get(self._endpoint('search'), params=params)
@@ -102,4 +244,80 @@ class Giphy(object):
         data = resp.json()
         self._check_or_raise(data.get('meta'))
 
-        return [self._make_result(result) for result in data['data']]
+        # TODO: Make this a generator
+        # TODO: Handle pagination after generator
+        return [GiphyResult(img) for img in data['data']]
+
+    def translate(self, term=None, phrase=None):
+        """
+        Retrieve a single image that represents a transalation of a term or
+        phrase into an animated gif. Punctuation is ignored. By default, this
+        will perform a `term` translation. If you want to translate by phrase,
+        use the `phrase` keyword argument.
+
+        :param term: Search term or terms
+        :type term: string
+        :param phrase: Search phrase
+        :type phrase: string
+        """
+        assert any((term, phrase)), u'You must supply a term or phrase to search'
+
+        # Phrases should have dashes and not spaces
+        if phrase:
+            phrase = phrase.replace(' ', '-')
+
+        params = {
+            's': term or phrase,
+            'api_key': self.api_key,
+        }
+
+        resp = requests.get(self._endpoint('translate'), params=params)
+        resp.raise_for_status()
+
+        data = resp.json()
+        self._check_or_raise(data.get('meta'))
+
+        return GiphyResult(data['data'])
+
+    def gif(self, gif_id):
+        """
+        Retrieves a specifc gif from giphy based on unique id
+
+        :param gif_id: Unique giphy gif ID
+        :type gif_id: string
+        """
+        params = {
+            'api_key': self.api_key,
+        }
+
+        resp = requests.get(self._endpoint(gif_id), params=params)
+        resp.raise_for_status()
+
+        data = resp.json()
+        self._check_or_raise(data.get('meta'))
+
+        return GiphyResult(data['data'])
+
+    def screensaver(self, tag=None):
+        """
+        Returns a random giphy image, optionally based on a search of a given tag.
+        Note that this method will both query for a screensaver image and fetch the
+        full details of that image (2 request calls)
+
+        :param tag: Tag to retrieve a screensaver image
+        :type tag: string
+        """
+        params = {
+            'api_key': self.api_key,
+        }
+
+        if tag:
+            params['tag'] = tag
+
+        resp = requests.get(self._endpoint('screensaver'), params=params)
+        resp.raise_for_status()
+
+        data = resp.json()
+        self._check_or_raise(data.get('meta'))
+
+        return self.gif(data['data']['id'])
--- tests.py
+++ tests.py
@@ -2,22 +2,162 @@ from unittest import TestCase
 
 from mock import Mock
 
-import giphypop
+from giphypop import Giphy, GiphyResult, AttrDict
 
 
-class GiphyTestCase(TestCase):
+class AttrDictTestCase(TestCase):
 
-    def setUp(self):
-        self.api = giphypy.Giphy()
+    def test_get_attribute_raises(self):
+        foo = AttrDict()
+        self.assertRaises(AttributeError, lambda: foo.bar)
 
-    def test_search(self):
-        self.api.search('popcorn')
+    def test_get_attribte(self):
+        foo = AttrDict(bar='baz')
+        assert foo.bar == 'baz'
 
-    def test_translate(self):
-        pass
+    def test_get_attribute_proxies_key(self):
+        foo = AttrDict(bar='baz')
+        foo['bar'] = 'baz'
+        assert foo.bar == 'baz'
 
-    def test_screensaver(self):
-        pass
+    def test_set_attribute(self):
+        foo = AttrDict()
+        foo.bar = 'baz'
+        assert foo.bar == 'baz'
 
-    def test_gif(self):
-        pass
+    def test_set_attribute_proxies_key(self):
+        foo = AttrDict()
+        foo.bar = 'baz'
+        assert foo['bar'] == 'baz'
+
+    def test_hasattr(self):
+        foo = AttrDict(bar='baz')
+        assert hasattr(foo, 'bar')
+        assert not hasattr(foo, 'baz')
+
+    def test_with_property(self):
+        class Foo(AttrDict):
+            @property
+            def foo(self):
+                return 'bar'
+
+        foo = Foo()
+        assert foo.foo == 'bar'
+
+
+class GiphyResultCase(TestCase):
+
+    def test_normalize(self):
+        result = GiphyResult()
+        norm = result._normalized({
+            'width': '200',
+            'height': 300,
+            'something': 'foo',
+            'frames': '100',
+            'size': '1234567890'
+        })
+
+        assert isinstance(norm['width'], int)
+        assert isinstance(norm['height'], int)
+        assert isinstance(norm['frames'], int)
+        assert isinstance(norm['size'], int)
+        assert isinstance(norm['something'], basestring)
+
+    def test_make_images_creates_attribute(self):
+        # Expect that make_images will create an attribute with key name
+        result = GiphyResult()
+        img = {'original': FAKE_DATA['images']['original']}
+
+        assert not hasattr(result, 'original')
+        result._make_images(img)
+        assert hasattr(result, 'original')
+
+    def test_make_images_doesnt_subattr(self):
+        # If there is a single underscore, don't subattr
+        result = GiphyResult()
+        img = {'fixed_width': FAKE_DATA['images']['fixed_width']}
+
+        assert not hasattr(result, 'fixed')
+        assert not hasattr(result, 'fixed_width')
+        result._make_images(img)
+        assert not hasattr(result, 'fixed')
+        assert hasattr(result, 'fixed_width')
+
+    def test_make_images_creates_subattr(self):
+        result = GiphyResult()
+        img = {'fixed_width': FAKE_DATA['images']['fixed_width'],
+               'fixed_width_still': FAKE_DATA['images']['fixed_width_still']}
+
+        assert not hasattr(result, 'fixed_width')
+        result._make_images(img)
+        assert hasattr(result, 'fixed_width')
+        assert hasattr(result.fixed_width, 'still')
+
+    def test_original_properties(self):
+        result = GiphyResult()
+        img = {'original': FAKE_DATA['images']['original']}
+        props = {
+            'media_url': 'url',
+            'frames': 'frames',
+            'width': 'width',
+            'height': 'height',
+            'filesize': 'size'
+        }
+
+        for prop in props:
+            self.assertRaises(AttributeError, lambda: getattr(result, prop))
+
+        result._make_images(img)
+
+        for prop, attr in props.items():
+            assert getattr(result, prop) == getattr(result.original, attr)
+
+
+# TEST DATA
+FAKE_DATA = {
+    "bitly_fullscreen_url": "http://gph.is/XH7Sri",
+    "bitly_gif_url": "http://gph.is/XH7V6j",
+    "bitly_tiled_url": "http://gph.is/XH7Srk",
+    "id": "3avUsGhmckIYE",
+    "images": {
+        "fixed_height": {
+            "height": "200",
+            "url": "http://media.giphy.com/media/3avUsGhmckIYE/200.gif",
+            "width": "289"
+        },
+        "fixed_height_downsampled": {
+            "height": "200",
+            "url": "http://media.giphy.com/media/3avUsGhmckIYE/200_d.gif",
+            "width": "289"
+        },
+        "fixed_height_still": {
+            "height": "200",
+            "url": "http://media.giphy.com/media/3avUsGhmckIYE/200_s.gif",
+            "width": "289"
+        },
+        "fixed_width": {
+            "height": "138",
+            "url": "http://media.giphy.com/media/3avUsGhmckIYE/200w.gif",
+            "width": "200"
+        },
+        "fixed_width_downsampled": {
+            "height": "138",
+            "url": "http://media.giphy.com/media/3avUsGhmckIYE/200w_d.gif",
+            "width": "200"
+        },
+        "fixed_width_still": {
+            "height": "138",
+            "url": "http://media.giphy.com/media/3avUsGhmckIYE/200w_s.gif",
+            "width": "200"
+        },
+        "original": {
+            "height": "346",
+            "url": "http://media.giphy.com/media/3avUsGhmckIYE/giphy.gif",
+            "width": "500",
+            "frames": "100",
+            "size": "123",
+        }
+    },
+    "type": "gif",
+    "url": "http://giphy.com/gifs/3avUsGhmckIYE"
+}
